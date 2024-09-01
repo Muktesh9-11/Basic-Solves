53d61cde84d539846acb610d32d69457af7fa29f
Muktesh Bodkhe <mukteshbodkhe04@gmail.com>
2024-09-02 01:20:56 +0530

    Time: 0 ms (100%), Space: 12.1 MB (69.1%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
-    int i;
-    bool solve(vector<int>& arr,int low,int high, int target){
-        int mid=(low+high)/2;
+    bool solve(vector<vector<int>>& matrix,int low,int high,int target,int n){
+        int mid=low+(high-low)/2;
         if(low>high) return false;
-        if(arr[mid]==target) return true;
-        else if(arr[mid]<target) return solve(arr,mid+1,high,target);
-        else return solve(arr,low,mid-1,target);
-    }
-
-    void check(vector<vector<int>>& arr,int low,int high,int target){
-        int mid=(low+high)/2;
-        if(low>high) return;
-        if(arr[mid][0]<=target){
-            i=mid;
-            return check(arr,mid+1,high,target);
-        }
-        return check(arr,low,mid-1,target);
+        int row=mid/n;
+        int col=mid%n;
+        if(matrix[row][col]==target)return true;
+        else if(matrix[row][col]<target) return solve(matrix,mid+1,high,target,n);
+        else return solve(matrix,low,mid-1,target,n);
     }
 
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        //row= i/n,   column=i%n
         int m=matrix.size();
         int n=matrix[0].size();
-        int ans=-1;
-        check(matrix,0,m-1,target);
-        if(solve(matrix[i],0,n-1,target)) return true;
-       /* for(int i=0;i<m;i++){
-            if(solve(matrix[i],0,n-1,target))return true;
-        }*/
-
-        return false;
+        return solve(matrix,0,(n*m)-1,target,n);
     }
 };
