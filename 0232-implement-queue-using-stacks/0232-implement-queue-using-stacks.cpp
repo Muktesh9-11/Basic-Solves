9e58d21da182771efc9e80f9b0e8c05616b6de95
Muktesh Bodkhe <mukteshbodkhe04@gmail.com>
2024-09-18 00:15:47 +0530

    Time: 0 ms (100%), Space: 8.8 MB (22.64%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,35 +1,55 @@
 class MyQueue {
-private:
-    stack<int> s1;
-    stack<int> s2;
-
 public:
-    MyQueue() {}
-
+    stack<int> s;
+    stack<int>s2;
+    MyQueue() {
+        
+    }
+    
     void push(int x) {
-        while (!s1.empty()) {
-            s2.push(s1.top());
-            s1.pop();
+        s.push(x);
+    }
+    
+    int pop() {
+        if(s.size()==0) return -1;
+        //int flag=0;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            int temp=s.top();
+            s2.push(temp);
+            s.pop();
         }
-        s1.push(x);
-        while (!s2.empty()) {
-            s1.push(s2.top());
+        int x=s2.top();
+        s2.pop();
+        for(int i=0;i<n-1;i++){
+            int temp=s2.top();
+            s.push(temp);
             s2.pop();
         }
-    }
+        return x;
 
-    int pop() {
-        int temp = s1.top();
-        s1.pop();
-        return temp;
     }
-
+    
     int peek() {
-        return s1.top();
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            int temp=s.top();
+            s2.push(temp);
+            s.pop();
+        }
+        int x=s2.top();
+        //s2.pop();
+        for(int i=0;i<n;i++){
+            int temp=s2.top();
+            s.push(temp);
+            s2.pop();
+        }
+        return x;
     }
-
+    
     bool empty() {
-        return s1.empty();
+        if(s.empty()) return true;
+        return false;
     }
 };
 
