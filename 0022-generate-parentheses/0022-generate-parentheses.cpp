0294d26d1d774889bff80f04842b830a2a3b8ec5
Muktesh Bodkhe <mukteshbodkhe04@gmail.com>
2024-09-24 21:42:16 +0530

    Time: 4 ms (43.82%), Space: 15.5 MB (31.66%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
 
-    vector<string>answer;
+    vector<string> arr;
 
-    void backtrack(int o,int c,string current){
-        if(o==0 && c==0){
-            answer.push_back(current);
-            return;
+    void solve(string s,int left,int right){
+        if(left==0 && right==0){
+            arr.push_back(s);
+            //return;
         }
-
-        if(o>0) backtrack(o-1,c,current+"(");
-
-        if(c>o){
-            backtrack(o,c-1,current+")");
+        if(left>0) {
+            solve(s+'(',left-1,right);
+        }
+        //left++;
+        if(right>left){
+            solve(s+')',left,right-1);
         }
     }
 
     vector<string> generateParenthesis(int n) {
-        
-        backtrack(n,n,"");
-        return answer;
+        solve("",n,n);
+        return arr;
+
     }
 };
