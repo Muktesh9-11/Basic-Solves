5a94ad71a6bfb4a0ff810004ee7eb6d234216f4a
Muktesh Bodkhe <mukteshbodkhe04@gmail.com>
2025-03-04 00:02:19 +0530

    Time: 46 ms (38.01%), Space: 79.1 MB (9.07%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,61 +1,23 @@
 class Solution {
 public:
     int totalFruit(vector<int>& arr) {
-        int a,b;
+        int l = 0,r=0, maxlen = 0;
+        map<int,int> mpp;
         int n = arr.size();
-        int ans = 0;
-        int counta = 0,countb = 0;
-        int i = 0,j = 0;
 
-        if(n == 1){
-            return 1;
-        }
-        else if(n==2){
-            return 2;
-        }
-
-        for(i = 0;i<n;i++){
-            if(arr[i] != arr[j]){
-                j = i-1;
-                counta = 1;
-                break;
+        while(r<n){
+            mpp[arr[r]]++;
+            if(mpp.size() > 2){
+                mpp[arr[l]] -- ;
+                if(mpp[arr[l]] == 0) mpp.erase(arr[l]);
+                l++;
             }
+            if(mpp.size() <= 2){
+                maxlen = r-l+1;
+               // r++;
+            }
+            r++;
         }
-        countb = i;
-        //if(i<n) 
-        a = arr[i];
-        b = arr[j];
-        int k = i+1;
-        ans = max(ans,counta+countb);
-        while(k<n){
-            if(arr[k] == a){
-                i = k;
-                counta++;
-                ans = max(ans,counta+countb);
-            }
-            else if(arr[k] == b){
-                j = k;
-                countb++;
-                ans = max(ans,counta+countb);
-            }
-            else{
-                if(i<j){
-                    countb = j-i;
-                    i = k;
-                    a = arr[i];
-                    counta = 1;
-                    ans = max(ans,counta+countb);
-                }
-                else{
-                    counta = i-j;
-                    j = k;
-                    b = arr[j];
-                    countb = 1;
-                    ans = max(ans,counta+countb);
-                }
-            }
-            k++;
-        }
-        return ans;
+        return maxlen;
     }
 };
