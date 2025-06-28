376a89bede6c3368ed445a78b8d7632bb6296ca3
Muktesh Bodkhe <mukteshbodkhe04@gmail.com>
2025-06-28 12:34:20 +0530

    Time: 0 ms (100%), Space: 14.8 MB (76.36%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -3,24 +3,26 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
 
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        vector<int>dp(n,-1);
 
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i==0) {
-                    dp[i][j] = matrix[i][j];
-                    continue;
-                }
+            dp[i] = matrix[0][i];
+        }
 
-                if(j==0) dp[i][j] = min(dp[i-1][j], dp[i-1][j+1]) + matrix[i][j];
-                else if(j==n-1) dp[i][j] = min(dp[i-1][j] , dp[i-1][j-1]) + matrix[i][j];
-                else dp[i][j] = min(dp[i-1][j] , min(dp[i-1][j-1] , dp[i-1][j+1])) + matrix[i][j];
+        for(int i=1;i<n;i++){
+            vector<int> temp(n,-1);
+            for(int j=0;j<n;j++){
+
+                if(j==0) temp[j] = min(dp[j], dp[j+1]) + matrix[i][j];
+                else if(j==n-1) temp[j] = min(dp[j] , dp[j-1]) + matrix[i][j];
+                else temp[j] = min(dp[j] , min(dp[j-1] , dp[j+1])) + matrix[i][j];
 
             }
+            dp = temp;
         }
 
         int ans = INT_MAX;
-        for(auto it : dp[n-1]){
+        for(auto it : dp){
             ans = min(ans,it);
         }
 
