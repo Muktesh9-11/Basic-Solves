b8857569ac7fa63a82994afd84e38c34d7948fa7
Muktesh Bodkhe <mukteshbodkhe04@gmail.com>
2025-07-12 10:52:25 +0530

    Time: 15 ms (75.01%), Space: 10.1 MB (91.32%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,3 +1,4 @@
+#define ll long long 
 class Solution {
 public:
     int n;
@@ -22,8 +23,27 @@ public:
 
     int change(int amount, vector<int>& coins) {
         n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        //vector<vector<ll>>dp(n+1,vector<ll>(amount+1,0));
 
-        return solve(coins,amount,0,dp);
+        vector<int> prev(amount+1,0), curr(amount+1,0);
+
+        for(int j=0;j<=amount;j++) prev[j] = (j%coins[0] == 0); 
+
+        //return solve(coins,amount,0,dp);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                ll notpick = prev[j];
+                ll pick = 0;
+                if(coins[i] <= j){
+                    pick = curr[j-coins[i]];
+                }
+
+                curr[j] = pick+notpick;
+            }
+            prev=curr;
+        }
+
+        return prev[amount];
+
     }
 };
