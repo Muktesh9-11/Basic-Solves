bde6158a5328deec83a03198793dde92d92d0897
Muktesh Bodkhe <mukteshbodkhe04@gmail.com>
2026-06-16 11:27:19 +0530

    3558-number-of-ways-to-assign-edge-weights-i 06-16-2026 Medium

--- LeetCode/Medium/3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.cpp
+++ LeetCode/Medium/3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.cpp
@@ -1,38 +1,44 @@
+#define pb push_back
+#define ll long long
+
 class Solution {
 public:
-    static const int MOD = 1e9 + 7;
 
-    int dfs(vector<vector<int>>& adj, int node, int par) {
+    int n;
+    int dfs(vector<vector<int>>&adj, int node, vector<int> &vis){
+        //if(adj[node].size() == 1) return 0;
+
         int depth = 0;
-
-        for (auto &child : adj[node]) {
-            if (child == par) continue;
-            depth = max(depth, 1 + dfs(adj, child, node));
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                vis[it] = 1;
+                depth = max(depth, 1 + dfs(adj,it,vis));
+            }
         }
 
         return depth;
     }
 
     int assignEdgeWeights(vector<vector<int>>& edges) {
-        int n = edges.size() + 1;
-        vector<vector<int>> adj(n + 1);
+        n = edges.size()+1;
+        vector<vector<int>> adj(n+1);   
 
-        for (auto &e : edges) {
-            int u = e[0];
-            int v = e[1];
-
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        for(auto& it : edges){
+            adj[it[0]].pb(it[1]);
+            adj[it[1]].pb(it[0]);
         }
 
-        int depth = dfs(adj, 1, -1);
+        vector<int> vis(n+1,0);
+        vis[1] = 1;
 
-        long long res = 1;
+        int depth = dfs(adj,1,vis);
 
-        for (int i = 1; i < depth; i++) {
-            res = (res * 2) % MOD;
+        ll ans = 1;
+        for(int i=1; i<depth; i++){
+            ans = (ans*2)%(1000000007);
         }
 
-        return res;
+        return ans;
+
     }
 };
