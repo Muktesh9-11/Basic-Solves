d45aafa2396bf5de1f0ab6fd2c876b100e23b0f0
Muktesh Bodkhe <mukteshbodkhe04@gmail.com>
2026-07-03 14:54:39 +0530

    0084-largest-rectangle-in-histogram 07-03-2026 Hard

---
+++ LeetCode/Hard/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+                int n = heights.size();
+
+
+        vector<int>left(n);
+        vector<int> right(n);
+        stack<int> st;
+
+        for(int i=0;i<n;i++){
+            while(!st.empty() && heights[i] <= heights[st.top()]){
+                st.pop();
+            }
+            left[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+
+        while(!st.empty()) st.pop();
+
+        for(int i= n-1 ; i>=0; i--){
+            while(!st.empty() && heights[i] <= heights[st.top()]){
+                st.pop();
+            }
+            right[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            int width = right[i] - left[i] -1;
+            ans = max(ans , heights[i]*width);
+        }
+
+        return ans;
+    }
+};
